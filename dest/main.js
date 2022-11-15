de1d1e73cef204555bc685970ab6f94eec8f2a52
Trung Phạm <trung08052000@gmail.com>
2022-11-15 18:20:54 +0700

    new update

--- dest/main.js
+++ dest/main.js
@@ -1,124 +1,41 @@
 const body = $("body");
+const main = $(".container");
+Splitting();
 $(document).ready(function () {
-  $(".button").on("click", function () {
+  //open or close menu
+  $(".navbar .button").on("click", function () {
     $(".navbar").toggleClass("active");
     $(".header").toggleClass("activeHeader");
     const buttonName = $(".navbar .title")[0];
     if ($(".navbar").hasClass("active")) {
       buttonName.innerHTML = "Close";
-      body.addClass("disableScroll");
+      main.addClass("disableScroll");
     } else {
       buttonName.innerHTML = "Menu";
-      body.removeClass("disableScroll");
+      main.removeClass("disableScroll");
     }
   });
 });
-// let cursor = document.querySelector(".cursor");
-// document.addEventListener("mousemove", (e) => {
-//   cursor.setAttribute(
-//     "style",
-//     "top: " + (e.screenY - 125) + "px; left:" + e.screenX + "px;"
-//   );
-// });
-// function hoverElement() {
-//   var cursor = $(".cursor");
-//   var logo = $(".logo");
-//   var image = $(".image");
-//   var swipe = $(".cursor .swipe");
-//   var email = $(".email");
-//   var nav = $(".button");
-//   //logo
-//   logo.mouseover(function () {
-//     cursor.addClass("hover-logo");
-//   });
-//   logo.mouseout(function () {
-//     cursor.removeClass("hover-logo");
-//   });
-//   //image
-//   image.mouseover(function () {
-//     cursor.addClass("hover-info");
-//     swipe.addClass("active-swipe");
-//   });
-//   image.mouseout(function () {
-//     cursor.removeClass("hover-info");
-//     swipe.removeClass("active-swipe");
-//   });
-//   //email
-//   email.mouseover(function () {
-//     cursor.addClass("hover-email");
-//   });
-//   email.mouseout(function () {
-//     cursor.removeClass("hover-email");
-//   });
-//   //nav
-//   nav.mouseover(function () {
-//     cursor.addClass("hover-menu");
-//   });
-//   nav.mouseout(function () {
-//     cursor.removeClass("hover-menu");
-//   });
-// }
-// hoverElement();
-var activeInfo = $(".info .image");
+//open information
+var activeInfo = $(".introduce .introduce_title-about");
 activeInfo.on("click", function () {
   $(".profile").addClass("activeInfo");
   body.addClass("disableScroll");
 });
-//escape
+//escape to close information
 $(document).keyup(function (e) {
   if (e.key === "Escape") {
     $(".profile").removeClass("activeInfo");
     body.removeClass("disableScroll");
   }
 });
+//click button to close information
+$(".history .button").on("click", function () {
+  $(".profile").removeClass("activeInfo");
+  body.removeClass("disableScroll");
+});
 
-// setTimeout(() => {
-//   // Wrap every letter in a span
-//   var textWrapper = document.querySelector(".hello .letters");
-//   textWrapper.innerHTML = textWrapper.textContent.replace(
-//     /\S/g,
-//     "<span class='letter'>$&</span>"
-//   );
-//   anime.timeline({ loop: false }).add({
-//     targets: ".hello .letter",
-//     translateY: ["1.2em", 0],
-//     translateZ: 0,
-//     duration: 2000,
-//     delay: (el, i) => 50 * i,
-//   });
-// }, 500);
-// setTimeout(() => {
-//   // Wrap every letter in a span
-//   var textWrapper = document.querySelector(".mainTitle .letters");
-//   textWrapper.innerHTML = textWrapper.textContent.replace(
-//     /\S/g,
-//     "<span class='letter'>$&</span>"
-//   );
-//   anime.timeline({ loop: false }).add({
-//     targets: ".mainTitle .letter",
-//     translateY: ["1.2em", 0],
-//     translateZ: 0,
-//     duration: 2000,
-//     delay: (el, i) => 50 * i,
-//   });
-// }, 500);
-// setTimeout(() => {
-//   // Wrap every letter in a span
-//   var textWrapper = document.querySelector(".role .letters");
-//   textWrapper.innerHTML = textWrapper.textContent.replace(
-//     /\S/g,
-//     "<span class='letter'>$&</span>"
-//   );
-//   anime.timeline({ loop: false }).add({
-//     targets: ".role .letter",
-//     translateY: ["1.2em", 0],
-//     translateZ: 0,
-//     duration: 800,
-//     delay: (el, i) => 50 * i,
-//   });
-// }, 500);
-
-//cusor
+//cusor Gsap
 let cursor = document.querySelector(".cursor");
 let more = document.querySelector(".cursor .more");
 let cursorScale = document.querySelectorAll(".cursor-scale");
