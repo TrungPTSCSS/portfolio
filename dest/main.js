46964a3244d4bec6e373449f386d1f7a9e8a26cf
Trung Phạm <trung08052000@gmail.com>
2022-11-15 19:07:28 +0700

    update new code

--- dest/main.js
+++ dest/main.js
@@ -1,6 +1,6 @@
+Splitting();
 const body = $("body");
 const main = $(".container");
-Splitting();
 $(document).ready(function () {
   //open or close menu
   $(".navbar .button").on("click", function () {
@@ -15,65 +15,67 @@ $(document).ready(function () {
       main.removeClass("disableScroll");
     }
   });
-});
-//open information
-var activeInfo = $(".introduce .introduce_title-about");
-activeInfo.on("click", function () {
-  $(".profile").addClass("activeInfo");
-  body.addClass("disableScroll");
-});
-//escape to close information
-$(document).keyup(function (e) {
-  if (e.key === "Escape") {
+  //open information
+  var activeInfo = $(".introduce .introduce_title-about");
+  activeInfo.on("click", function () {
+    $(".profile").addClass("activeInfo");
+    body.addClass("disableScroll");
+  });
+  //escape to close information
+  $(document).keyup(function (e) {
+    if (e.key === "Escape") {
+      $(".profile").removeClass("activeInfo");
+      body.removeClass("disableScroll");
+    }
+  });
+  //click button to close information
+  $(".history .button").on("click", function () {
     $(".profile").removeClass("activeInfo");
     body.removeClass("disableScroll");
-  }
-});
-//click button to close information
-$(".history .button").on("click", function () {
-  $(".profile").removeClass("activeInfo");
-  body.removeClass("disableScroll");
-});
+  });
 
-//cusor Gsap
-let cursor = document.querySelector(".cursor");
-let more = document.querySelector(".cursor .more");
-let cursorScale = document.querySelectorAll(".cursor-scale");
-let mouseX = 0;
-let mouseY = 0;
+  //cusor Gsap
+  let cursor = document.querySelector(".cursor");
+  let more = document.querySelector(".cursor .more");
+  let cursorScale = document.querySelectorAll(".cursor-scale");
+  let mouseX = 0;
+  let mouseY = 0;
 
-gsap.to({}, 0.016, {
-  repeat: -1,
-  onRepeat: function () {
-    gsap.set(cursor, {
-      css: {
-        left: mouseX,
-        top: mouseY,
-      },
+  gsap.to({}, 0.016, {
+    repeat: -1,
+    onRepeat: function () {
+      gsap.set(cursor, {
+        css: {
+          left: mouseX,
+          top: mouseY,
+        },
+      });
+    },
+  });
+
+  window.addEventListener("mousemove", (e) => {
+    mouseX = e.clientX + 10;
+    mouseY = e.clientY + 10;
+  });
+
+  cursorScale.forEach((link) => {
+    link.addEventListener("mousemove", () => {
+      cursor.classList.add("grow");
+      if (link.classList.contains("small")) {
+        cursor.classList.remove("grow");
+        cursor.classList.add("grow-small");
+      }
+      if (link.classList.contains("image")) {
+        more.classList.add("activeMore");
+      }
     });
-  },
-});
 
-window.addEventListener("mousemove", (e) => {
-  mouseX = e.clientX + 10;
-  mouseY = e.clientY + 10;
-});
-
-cursorScale.forEach((link) => {
-  link.addEventListener("mousemove", () => {
-    cursor.classList.add("grow");
-    if (link.classList.contains("small")) {
+    link.addEventListener("mouseleave", () => {
       cursor.classList.remove("grow");
-      cursor.classList.add("grow-small");
-    }
-    if (link.classList.contains("image")) {
-      more.classList.add("activeMore");
-    }
-  });
-
-  link.addEventListener("mouseleave", () => {
-    cursor.classList.remove("grow");
-    cursor.classList.remove("grow-small");
-    more.classList.remove("activeMore");
+      cursor.classList.remove("grow-small");
+      more.classList.remove("activeMore");
+    });
   });
 });
+AOS.init();
+
