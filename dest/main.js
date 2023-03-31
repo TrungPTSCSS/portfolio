a653d804c981225fb952d899a4c36c06fb087710
Pham Thanh Trung <56735036+TrungPTSCSS@users.noreply.github.com>
2023-03-31 09:02:25 +0700

    update email

--- dest/main.js
+++ dest/main.js
@@ -1,52 +1,52 @@
 AOS.init();
 Splitting();
-const body = $("body");
-const main = $(".container");
+const body = $('body');
+const main = $('.container');
 $(document).ready(function () {
   //open or close menu
-  $(".navbar .button").on("click", function () {
-    $(".navbar").toggleClass("active");
-    $(".header").toggleClass("activeHeader");
-    const buttonName = $(".navbar .title")[0];
-    if ($(".navbar").hasClass("active")) {
-      buttonName.innerHTML = "Close";
-      main.addClass("disableScroll");
+  $('.navbar .button').on('click', function () {
+    $('.navbar').toggleClass('active');
+    $('.header').toggleClass('activeHeader');
+    const buttonName = $('.navbar .title')[0];
+    if ($('.navbar').hasClass('active')) {
+      buttonName.innerHTML = 'Close';
+      main.addClass('disableScroll');
     } else {
-      buttonName.innerHTML = "Menu";
-      main.removeClass("disableScroll");
+      buttonName.innerHTML = 'Menu';
+      main.removeClass('disableScroll');
     }
   });
   //open information
-  var activeInfo = $(".introduce .introduce_title-about");
-  activeInfo.on("click", function () {
-    $(".profile").addClass("activeInfo");
-    body.addClass("disableScroll");
+  var activeInfo = $('.introduce .introduce_title-about');
+  activeInfo.on('click', function () {
+    $('.profile').addClass('activeInfo');
+    body.addClass('disableScroll');
   });
   //escape to close information
   $(document).keyup(function (e) {
-    if (e.key === "Escape") {
-      $(".profile").removeClass("activeInfo");
-      body.removeClass("disableScroll");
+    if (e.key === 'Escape') {
+      $('.profile').removeClass('activeInfo');
+      body.removeClass('disableScroll');
     }
   });
   //click button to close information
-  $(".history .button").on("click", function () {
-    $(".profile").removeClass("activeInfo");
-    body.removeClass("disableScroll");
+  $('.history .button').on('click', function () {
+    $('.profile').removeClass('activeInfo');
+    body.removeClass('disableScroll');
   });
 
-  window.addEventListener("scroll", function () {
+  window.addEventListener('scroll', function () {
     var scrollHeight = document.documentElement.scrollTop;
     if (scrollHeight > window.innerHeight * 7) {
       //add class animation signature
-      let sign = document.querySelector(".signature svg path");
-      sign.classList.add("animation-text");
+      let sign = document.querySelector('.signature svg path');
+      sign.classList.add('animation-text');
     }
   });
   //cusor Gsap
-  let cursor = document.querySelector(".cursor");
-  let more = document.querySelector(".cursor .more");
-  let cursorScale = document.querySelectorAll(".cursor-scale");
+  let cursor = document.querySelector('.cursor');
+  let more = document.querySelector('.cursor .more');
+  let cursorScale = document.querySelectorAll('.cursor-scale');
   let mouseX = 0;
   let mouseY = 0;
 
@@ -62,38 +62,48 @@ $(document).ready(function () {
     },
   });
 
-  window.addEventListener("mousemove", (e) => {
+  window.addEventListener('mousemove', (e) => {
     mouseX = e.clientX + 10;
     mouseY = e.clientY + 10;
   });
 
   cursorScale.forEach((link) => {
-    link.addEventListener("mousemove", () => {
-      cursor.classList.add("grow");
-      if (link.classList.contains("small")) {
-        cursor.classList.remove("grow");
-        cursor.classList.add("grow-small");
+    link.addEventListener('mousemove', () => {
+      cursor.classList.add('grow');
+      if (link.classList.contains('small')) {
+        cursor.classList.remove('grow');
+        cursor.classList.add('grow-small');
       }
-      if (link.classList.contains("showMore")) {
-        more.classList.add("activeMore");
+      if (link.classList.contains('showMore')) {
+        more.classList.add('activeMore');
       }
     });
 
-    link.addEventListener("mouseleave", () => {
-      cursor.classList.remove("grow");
-      cursor.classList.remove("grow-small");
-      more.classList.remove("activeMore");
+    link.addEventListener('mouseleave', () => {
+      cursor.classList.remove('grow');
+      cursor.classList.remove('grow-small');
+      more.classList.remove('activeMore');
     });
   });
 
-  var parent = $(".content").get();
-    var all = parent[0].children;
+  var parent = $('.content').get();
+  var all = parent[0].children;
 
-    for (let i = 0; i < all.length; i++) {
-      all[i].addEventListener("click", function () {
-        console.log("====================================");
-        console.log(i);
-        console.log("====================================");
-      });
-    }
+  for (let i = 0; i < all.length; i++) {
+    all[i].addEventListener('click', function () {
+      console.log('====================================');
+      console.log(i);
+      console.log('====================================');
+    });
+  }
+  $('.email').on('click', function () {
+    $(location).attr(
+      'href',
+      'mailto:trung08052000@gmail.com?subject=' +
+        encodeURIComponent('This is my subject') +
+        '&body=' +
+        encodeURIComponent('This is my body')
+    );
+    return false;
+  });
 });
